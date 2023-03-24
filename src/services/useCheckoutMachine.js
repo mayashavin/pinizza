a4fcd415c190e3a9e17e66ad4ceea68ad68da79c
Maya Shavin <dpnminh@gmail.com>
2023-03-24 01:35:14 +0100

    remove: unused code

--- src/services/useCheckoutMachine.js
+++ src/services/useCheckoutMachine.js
@@ -3,7 +3,7 @@
 import { interpret } from "xstate";
 import { checkoutMachine } from "../machines/checkoutMachine.js";
 
-import { useActor, useService } from '@xstate/vue'
+import { useActor } from '@xstate/vue'
 
 const checkoutService = interpret(checkoutMachine).start()
 
